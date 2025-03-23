7836c75beb36952b64725a3b6a975bedf0af99c5
Rekha Shewale <rekhashewale94@gmail.com>
2025-03-23 20:16:05 +0530

    Bucket name changed-Rekha

--- workflows/pyspark_dag.py
+++ workflows/pyspark_dag.py
@@ -13,7 +13,7 @@ from airflow.providers.google.cloud.operators.dataproc import (
 PROJECT_ID = "rekha-gcp-practice-15022025"
 REGION = "us-east1"
 CLUSTER_NAME = "my-demo-cluster"
-COMPOSER_BUCKET = "us-central1-demo-instance-968eb987-bucket"
+COMPOSER_BUCKET = "us-central1-demo-instance-4d6395ae-bucket"
 
 GCS_JOB_FILE_1 = f"gs://{COMPOSER_BUCKET}/data/INGESTION/retailerMysqlToLanding.py"
 PYSPARK_JOB_1 = {
@@ -38,7 +38,7 @@ PYSPARK_JOB_3 = {
 
 
 ARGS = {
-    "owner": "SHAIK SAIDHUL",
+    "owner": "REKHA SHEWALE",
     "start_date": None,
     "depends_on_past": False,
     "email_on_failure": False,
