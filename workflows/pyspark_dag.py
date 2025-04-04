d71783d62a9486d0f310fa6c6bb739e9185abc50
Rekha Shewale <rekhashewale94@gmail.com>
2025-04-04 18:35:17 +0530

    .

--- workflows/pyspark_dag.py
+++ workflows/pyspark_dag.py
@@ -13,7 +13,7 @@ from airflow.providers.google.cloud.operators.dataproc import (
 PROJECT_ID = "rekha-gcp-practice-15022025"
 REGION = "us-east1"
 CLUSTER_NAME = "my-demo-cluster"
-COMPOSER_BUCKET = "us-central1-demo-instance-9a486fd1-bucket"
+COMPOSER_BUCKET = "us-central1-demo-instance-269ed9e4-bucket" 
 
 GCS_JOB_FILE_1 = f"gs://{COMPOSER_BUCKET}/data/INGESTION/retailerMysqlToLanding.py"
 PYSPARK_JOB_1 = {
